a72f29252c5d9c7598af5f494404cb3e3b36b8d7
Paul Knutson <31307312+catsymptote@users.noreply.github.com>
2026-02-19 12:54:15 +0100

    Added request counter

--- main.py
+++ main.py
@@ -2,19 +2,28 @@ from fastapi import FastAPI
 import requests
 import uvicorn
 
+
 app = FastAPI()
 
 
+REQUEST_COUNTER = 0
 CACHE = {}
 
 
 @app.get('/')
 def info():
+    REQUEST_COUNTER += 1
     return {'Info': 'Go to `/characters/<id: int>` to get results. E.g., `/characters/1052`.'}
+
+
+@app.get('/request_counter')
+def get_request_count():
+    return {'request count': REQUEST_COUNTER}
 
 
 @app.get("/characters/{id}")
 def get_characters(id: int):
+    REQUEST_COUNTER += 1
     # Cache hit — No API request required
     if id in CACHE:
         cache_hit = True
